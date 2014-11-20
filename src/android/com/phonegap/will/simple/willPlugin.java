771a6ff2fa61792c1502bd1b4a26f1d05de51caf
Luca Orsini <orsinil@inwind.it>
2014-11-20 10:57:49 +0100

    1

--- src/android/com/phonegap/will/simple/willPlugin.java
+++ src/android/com/phonegap/will/simple/willPlugin.java
@@ -1,4 +1,4 @@
-package com.phonegap.plugins.will;
+package com.phonegap.will.simple;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONObject;
